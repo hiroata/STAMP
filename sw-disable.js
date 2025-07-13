3275191be060d0302dc20e0fe9be7d1e5718c65e
World Stamp Hiroshima Developer <developer@worldstamp-hiroshima.com>
2025-07-13 20:13:42 +0900

    更新

--- sw-disable.js
+++
@@ -1,8 +0,0 @@
-// Service Workerを無効化するスクリプト
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(function(registrations) {
-    for(let registration of registrations) {
-      registration.unregister();
-    }
-  });
-}
